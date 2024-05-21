1ee0612ba6e3d5329bccd6edee59fa9829a5091f
epenflow <epenflow@gmail.com>
2024-05-21 15:07:40 +0800

    refactor

--- src/app/components/Profil.tsx
+++ src/app/components/Profil.tsx
@@ -3,7 +3,7 @@ import { GROOM_BIODATA, BRIDE_BIODATA } from '../constants';
 import Biodata from './Biodata';
 const Profil = () => {
 	return (
-		<div className='flex items-center flex-col gap-3'>
+		<div className='flex items-center flex-col gap-3 z-20'>
 			<Biodata {...GROOM_BIODATA} />
 			<span className='text-4xl font-serif'>&</span>
 			<Biodata {...BRIDE_BIODATA} />
--- src/app/sections/Main.tsx
+++ src/app/sections/Main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Profil from '../components/Profil';
 import Preposition from '../components/Preposition';
+import Image from 'next/image';
 const Main = () => {
 	return (
-		<section className='h-screen w-screen bg-golden-hour-white px-4 py-5 flex flex-col items-center gap-3 justify-between'>
-			<div className='flex flex-col items-center'>
+		<section className='h-screen w-screen bg-golden-hour-white px-4 py-5 flex flex-col items-center gap-3 justify-between relative'>
+			<div className='flex flex-col items-center z-20'>
 				<h1 className='capitalize text-4xl font-elegante'>
 					om swastiastu
 				</h1>
@@ -15,7 +16,20 @@ const Main = () => {
 				</p>
 			</div>
 			<Profil />
-			<Preposition />
+			<div className='flex flex-col items-center justify-center w-full z-20'>
+				<Preposition />
+				<span className='text-center font-serif'>
+					Atas kehadiran serta doa restu Bapak/Ibu/Saudara/I. kami
+					sekeluarga mengucapkan terima kasih.
+				</span>
+			</div>
+			<Image
+				src={'/yellow-rose.png'}
+				alt='yellow-rose'
+				width={500}
+				height={500}
+				className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 opacity-50'
+			/>
 		</section>
 	);
 };
