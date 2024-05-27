01d7354ecec0d6979aeb344c4026f293c813b3b9
epenflow <epenflow@gmail.com>
2024-05-27 21:23:08 +0800

    opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,25 @@ import { BRIDE_BIODATA, GROOM_BIODATA } from './constants';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: `${GROOM_BIODATA.nickname} & ${BRIDE_BIODATA.nickname}`,
-	description: `The Wedding of ${GROOM_BIODATA.nickname} & ${BRIDE_BIODATA.nickname}`,
+	title: `The Wedding of ${GROOM_BIODATA.nickname} & ${BRIDE_BIODATA.nickname}`,
+	description: `Your are invited to the wedding of ${GROOM_BIODATA.nickname} & ${BRIDE_BIODATA.nickname}`,
 	icons: {
 		icon: {
 			url: '/WEDDING ICON.svg',
 		},
+	},
+	openGraph: {
+		title: `The Wedding of ${GROOM_BIODATA.nickname} & ${BRIDE_BIODATA.nickname}`,
+		description: `You are invited to the wedding of ${GROOM_BIODATA.nickname} & ${BRIDE_BIODATA.nickname}`,
+		siteName: `${GROOM_BIODATA.nickname} & ${BRIDE_BIODATA.nickname}`,
+		type: 'website',
+		images: [
+			{
+				url: 'RV_04170.JPG',
+				width: 800,
+				height: 600,
+			},
+		],
 	},
 };
 
@@ -22,16 +35,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<head>
-				<meta
-					property='og:image'
-					content='/RV_04170.JPG'
-				/>
-				<meta
-					property='og:title'
-					content={`${GROOM_BIODATA.nickname} & ${BRIDE_BIODATA.nickname}`}
-				/>
-			</head>
 			<body className={`${inter.className} ${elegante.variable}`}>
 				{children}
 			</body>
